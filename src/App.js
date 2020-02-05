ed137489695cf3472396a1269d7de3754d1c2c11
Nacho RM <joseignaciorm13@gmail.com>
2020-02-05 14:15:24 +0100

    Crea componente formulario, métodos para añadir y eliminar tareas

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Data
 import { todos } from './todos.json'
+
+// Subcomponents
+import TodoFrom from './Components/TodoForm'
 
 class App extends Component {
   constructor () {
@@ -10,22 +14,48 @@ class App extends Component {
     this.state = {
       todos
     }
+
+    this.handlerAddTodo = this.handlerAddTodo.bind(this)
+  }
+
+  handlerAddTodo (todo) {
+    this.setState({
+      todos: [ ...this.state.todos, todo ]
+    })
+  }
+
+  removeTodo (index) {
+    if ( window.confirm('Are you sure you want to delete it?') ) {
+      this.setState({
+        todos: this.state.todos.filter((e, i) => {
+          return i !== index
+        }) 
+      })
+    }
   }
 
   render () {
     const todos = this.state.todos.map(( todo, i ) => {
       return (
-        <div className="col-md-4">
+        <div className="col-md-4 text-center" key={i}>
           <div className="card mt-4">
-            <div className="card-header">
+            <div className="card-title">
               <h3>{todo.title}</h3>
               <span className="badge badge-pill badge-danger ml-2">
                 {todo.priority}
               </span>
             </div>
-            <div className="card-body">
+            <div className="card-body text-center">
               <p>{todo.description}</p>
               <mark>{todo.responsible}</mark>
+            </div>
+            <div className="card-footer">
+              <button
+                 className="btn btn-danger"
+                 onClick={this.removeTodo.bind(this, i)}
+              >
+                Delete
+              </button>
             </div>
           </div>
         </div>
@@ -42,14 +72,21 @@ class App extends Component {
             </span>
           </a>
         </nav>
-        
+
         <div className="container">
           <div className="row mt-4">
-            {todos}
+            <div className="col-md-3 text-center">
+              <img src={logo} className="App-logo" alt="logo" />
+              <TodoFrom onAddTodo={ this.handlerAddTodo } />              
+            </div>
+            <div className="col-md-9">
+              <div className="row">
+                {todos}
+              </div>
+            </div>
           </div>
         </div>
 
-        <img src={logo} className="App-logo" alt="logo" />
       </div>
     )
   }
